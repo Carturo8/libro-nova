efc48c3d3d01c63260ccf678080c3b27278d057f
Carturo <caarojasbo@unal.edu.co>
2025-10-14 10:03:29 -0500

    Add Main class to initialize authentication view

---
+++ src/main/java/com/libronova/app/Main.java
@@ -0,0 +1,10 @@
+package com.libronova.app;
+
+import com.libronova.view.AuthView;
+
+public class Main {
+    public static void main(String[] args) {
+        AuthView authView = new AuthView();
+        authView.showAuthMenu();
+    }
+}
